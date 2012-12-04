f28d8a718d040b906172f6833ed3ebbb13b1699a
mbarton <mitch_barton@yahoo.com>
2012-12-03 21:49:39 -0800

    Now fully tested speed/distance commands with new opcode 1 format (8 data bytes).
    Signed-off-by: mbarton <mitch_barton@yahoo.com>

--- MCECS Bot/Software/Motor_Packet_Controller/Motor_Packet_Controller.ino
+++ MCECS Bot/Software/Motor_Packet_Controller/Motor_Packet_Controller.ino
@@ -1,8 +1,8 @@
 /*  This program is designed to run on an Arduino Uno with 2 basic micro Roboclaw motor controllers connected, both running in packet serial mode.
-    It recieves commands from a serial port in a specified format, that is: fixed length, byte0 = 0xFF (dedicated), byte1 = 0xFF (dedicated), byte2 = opcode, 
-    bytes3-7 = motor command data.  This format is described in more detail in Jeramy's homework 3 found on the MCECS bot project page.
-    Version: 0.2
-*/
+ It recieves commands from a serial port in a specified format, that is: fixed length, byte0 = 0xFF (dedicated), byte1 = 0xFF (dedicated), byte2 = opcode, 
+ bytes3-7 = motor command data.  This format is described in more detail in Jeramy's homework 3 found on the MCECS bot project page.
+ Version: 0.2
+ */
 
 #include "BMSerial.h"
 #include "RoboClaw.h"
@@ -29,7 +29,7 @@ void setup() {
 
   // Baud rate of connection to motor driver
   roboclaw.begin(2400);
-  
+
   // ???
   pinMode(22, OUTPUT);
 
@@ -47,7 +47,7 @@ void displayspeed(void)
 {
   uint8_t status;
   bool valid;
-  
+
   uint32_t enc1= roboclaw.ReadEncM1(0x80, &status, &valid);
   if(valid){
     Serial.print("Encoder1:");
@@ -80,138 +80,162 @@ void displayspeed(void)
 }
 
 void loop() {
-  
+
   // initialize first two data bytes to be read in
   byte bytein0 = 0;  
   byte bytein1 = 0;
-  
+
   // initialize motor direction bits to be filled by the direction vector for case 0 (opcode 0 is basic motor control)
   byte M1dir;
   byte M2dir;
   byte M3dir;
   byte M4dir;
-  
-  
+
+
   // when serial data is available read in the first two bytes
   if(Serial.available() > 0)
   { 
     bytein0 = Serial.read();
     delay(100);  // this delay is apparently required to receive the data
     bytein1 = Serial.read();
-    
-    // if the first two bytes are the designated start of frame, 0xFFFF, then read in the data.  This needs to be a unique value w.r.t. the data
+
+    // if the first two bytes are the designated start of frame, 0xFFFF, then read in the data.  This needs to be a unique value w.r.t. t
+    // e data
     if(bytein0 == 0xFF && bytein1 == 0xFF)  
     {
-      // this loop reads each byte of the fixed length (6 bytes) message from the PC program into an array, after the start of frame header.  
+      // this loop reads each byte of the fixed length (6 bytes) message from the PC program into an array, after the start of frame header
+
       // Byte0 = opcode, Byte1-5 = motor command data. It may need to require that data is available, but it seems to work well as is.
-      for(int i = 0; i < 6; i++) {
-      datarray[i] = Serial.read();
+
+      datarray[0] = Serial.read();
       delay(100);
-      }   
-    
+
+
       //then apply the data to the roboclaw
       switch (datarray[0]) {
-        case 0:
+      case 0:
+        for(int i = 1; i < 6; i++) {
+          datarray[i] = Serial.read();
+          delay(100);
+        } 
         
-          // read direction vector into individual bits      
-          M1dir = bitRead(datarray[1],0);
-          M2dir = bitRead(datarray[1],1);
-          M3dir = bitRead(datarray[1],2);
-          M4dir = bitRead(datarray[1],3);
-      
-      
-          // print all of the data received
-          Serial.print("opcode: ");
-          Serial.println(datarray[0], DEC);
-          Serial.print("M1dir: ");
-          Serial.println(M1dir);
-          Serial.print("M2dir: ");
-          Serial.println(M2dir);
-          Serial.print("M3dir: ");
-          Serial.println(M3dir);
-          Serial.print("M4dir: ");
-          Serial.println(M4dir);
-          Serial.print("Motor1 speed: ");
-          Serial.println(datarray[2], DEC);
-          Serial.print("Motor2 speed: ");
-          Serial.println(datarray[3], DEC);
-          Serial.print("Motor3 speed: ");
-          Serial.println(datarray[4], DEC);
-          Serial.print("Motor4 speed: ");
-          Serial.println(datarray[5], DEC);        
-        
-          // motor 1 command depending on direction value
-          if(M1dir==1)
-          {
-          roboclaw.ForwardM1(0x80,datarray[2]);
-          }
-          else if(M1dir==0)
-          {
-          roboclaw.BackwardM1(0x80,datarray[2]);
-          }
-          
-          // motor 2 command depending on direction value
-          if(M2dir==1)
-          {
-          roboclaw.ForwardM2(0x80,datarray[3]);
-          }
-          else if(M2dir==0)
-          {
-          roboclaw.BackwardM2(0x80,datarray[3]);
-          } 
-           // motor 3 command depending on direction value
-          if(M3dir==1)
-          {
-          roboclaw.ForwardM1(0x81,datarray[4]);
-          }
-          else if(M3dir==0)
-          {
-          roboclaw.BackwardM1(0x81,datarray[4]);
-          }
-          
-          // motor 4 command depending on direction value
-          if(M4dir==1)
-          {
-          roboclaw.ForwardM2(0x81,datarray[5]);
-          }
-          else if(M4dir==0)
-          {
-          roboclaw.BackwardM2(0x81,datarray[5]);
-          }          
-          
-          break;
-          
-        case 1:
-          byte Mspeed;
-        
-          if(datarray[1]==15)
-          {
-            Mspeed = 24000;
-          }
-          else
-          {
-            Mspeed = 0;
-          }
-          
-          // motor 1 command
-          roboclaw.SpeedDistanceM1(0x80,Mspeed,datarray[2]);
-          /*uint8_t M1depth1,M1depth2;
-          do{
-            displayspeed();
-            roboclaw.ReadBuffers(0x80,M1depth1,M1depth2);
-          }while(M1depth1);
-*/
+        // read direction vector into individual bits      
+        M1dir = bitRead(datarray[1],0);
+        M2dir = bitRead(datarray[1],1);
+        M3dir = bitRead(datarray[1],2);
+        M4dir = bitRead(datarray[1],3);
 
-          // motor 3 command
-          roboclaw.SpeedDistanceM1(0x81,Mspeed,datarray[4]);
-          /*uint8_t M3depth1,M3depth2;
-          do{
-            displayspeed();
-            roboclaw.ReadBuffers(0x81,M3depth1,M3depth2);
-          }while(M3depth1);         
-          */
+
+        // print all of the data received
+        Serial.print("opcode: ");
+        Serial.println(datarray[0], DEC);
+        Serial.print("M1dir: ");
+        Serial.println(M1dir);
+        Serial.print("M2dir: ");
+        Serial.println(M2dir);
+        Serial.print("M3dir: ");
+        Serial.println(M3dir);
+        Serial.print("M4dir: ");
+        Serial.println(M4dir);
+        Serial.print("Motor1 speed: ");
+        Serial.println(datarray[2], DEC);
+        Serial.print("Motor2 speed: ");
+        Serial.println(datarray[3], DEC);
+        Serial.print("Motor3 speed: ");
+        Serial.println(datarray[4], DEC);
+        Serial.print("Motor4 speed: ");
+        Serial.println(datarray[5], DEC);        
+
+        // motor 1 command depending on direction value
+        if(M1dir==1)
+        {
+          roboclaw.ForwardM1(0x80,datarray[2]);
+        }
+        else if(M1dir==0)
+        {
+          roboclaw.BackwardM1(0x80,datarray[2]);
+        }
+
+        // motor 2 command depending on direction value
+        if(M2dir==1)
+        {
+          roboclaw.ForwardM2(0x80,datarray[3]);
+        }
+        else if(M2dir==0)
+        {
+          roboclaw.BackwardM2(0x80,datarray[3]);
+        } 
+        // motor 3 command depending on direction value
+        if(M3dir==1)
+        {
+          roboclaw.ForwardM1(0x81,datarray[4]);
+        }
+        else if(M3dir==0)
+        {
+          roboclaw.BackwardM1(0x81,datarray[4]);
+        }
+
+        // motor 4 command depending on direction value
+        if(M4dir==1)
+        {
+          roboclaw.ForwardM2(0x81,datarray[5]);
+        }
+        else if(M4dir==0)
+        {
+          roboclaw.BackwardM2(0x81,datarray[5]);
+        }          
+
+        break;
+
+      case 1:
+      
+        for(int i = 1; i < 5; i++) {
+          datarray[i] = Serial.read();
+          delay(100);
+        }
+        
+        
+        char M1speed = Serial.read();
+        delay(100);
+        char M2speed = Serial.read();
+        delay(100);
+        char M3speed = Serial.read();
+        delay(100);
+        char M4speed = Serial.read();
+        delay(100);
+        
+        Serial.print("M1speed = ");
+        Serial.println(M1speed,DEC);
+        Serial.print("M2speed = ");
+        Serial.println(M2speed,DEC);        
+        Serial.print("M3speed = ");
+        Serial.println(M3speed,DEC);
+        Serial.print("M4speed = ");
+        Serial.println(M4speed,DEC);
+        Serial.print("M1dist = ");
+        Serial.println(datarray[1]);
+        Serial.print("M2dist = ");
+        Serial.println(datarray[2]);        
+        Serial.print("M3dist = ");
+        Serial.println(datarray[3]);
+        Serial.print("M4dist = ");
+        Serial.println(datarray[4]);
+        
+        
+        
+        // motor 1 command
+        roboclaw.SpeedDistanceM1(0x80,M1speed*23,datarray[1]*2303);
+        displayspeed();
+        delay(100);
+
+        // motor 3 command
+        roboclaw.SpeedDistanceM1(0x81,M3speed*23,datarray[3]*2303);
+        
+        displayspeed();
+        delay(100);
+        
       }
-         
+
     }
     else  //the message was only partialy received, resend
     {
@@ -219,7 +243,8 @@ void loop() {
     }
 
     delay(100);
-    }
+  }
 }
+
 
 
