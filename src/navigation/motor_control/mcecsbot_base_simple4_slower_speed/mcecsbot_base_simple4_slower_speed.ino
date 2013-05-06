757a5518a33833ee215473bf1e86d58b3d11c152
Mathias Sunardi <m.sunardi@gmail.com>
2013-05-06 00:53:26 -0700

    Added current Arduino codes for base for the MEGA and UNO

---
+++ src/navigation/motor_control/mcecsbot_base_simple4_slower_speed/mcecsbot_base_simple4_slower_speed.ino
@@ -0,0 +1,487 @@
+/*  This program is designed to run on an Arduino Uno with 2 basic micro Roboclaw motor controllers connected, both running in packet serial mode.
+ It recieves commands from a serial port in a specified format, that is: fixed length, byte0 = 0xFF (dedicated), byte1 = 0xFF (dedicated), byte2 = opcode, 
+ bytes3-7 = motor command data.  This format is described in more detail in Jeramy's homework 3 found on the MCECS bot project page.
+ Version: 0.2
+ */
+#include "Wire.h" 
+#include "BMSerial.h" 
+#include "RoboClaw.h" 
+
+// define PID constants on roboclaw for speed/distance commands
+#define Kp 0x00010000
+#define Ki 0x00008000
+#define Kd 0x00004000
+#define qpps 44000
+#define sonar_controller 2
+#define STOP 4
+#define FORWARD 2
+#define BACKWARD 3
+#define RIGHT 0
+#define LEFT 1
+
+BMSerial terminalSerial(0,1);          // TerminalSerial is the debug serial windows when 0,1 is selected
+//RoboClaw roboclaw(50,52);              // 5,6 represent the pins on the arduino that the motor controller is connected to
+RoboClaw roboclaw(46,48);              // 5,6 represent the pins on the arduino that the motor controller is connected to
+
+    uint8_t MotorSpeed;                // Holds the current motorspeed of the robot
+    uint8_t TopMotorSpeed = 30;        // Top speed is actually TopMotorSpeed + 1
+    uint8_t NoMotorSpeed = 0;          // variable to name a speed of zero
+    uint8_t increment = 5;             // Rate of acceleration. Make sure it is a multiple of TopMotorSpeed
+    boolean stopmoving = false;        // Tells whether the robot has been asked to stop moving
+
+    boolean first_iteration = false;   // As the robot accelerates it only needs to send the Uno (sonar_controller)
+                                       // when it initiates a new movement. Otherwise the controller would be bogged
+                                       // down with unnecessary communication.   
+
+    boolean was_interrupt = false;     // If a sonar detects and object that is too close it drives a pin high. This
+                                       // variable stores the value of that interrupt pin (currently enacted as an 
+                                       // interrupt. The base code simply polls the "interrupt" pin).
+
+    int threshold = 15;                // Threshold. Holds the distance at which a sonar will generate an "interrupt" 
+                                       // Threshold must be the same value on the Uno(sonar_controller).    
+
+    int available_movements = 4;       // This is the number of movements available to the robot. Currently, 
+                                       // there is only forward, backward, rotate left, and rotate right. If you add
+                                       // any movement increment this by one. It is used to make sure nothing happens
+                                       // if incorrect input values are received. 
+
+    int new_movement = 5;              // Holds the command value from any input device telling what movement to make. 
+    int old_movement = 6;              // Holds the last value of new_movement. Used to transition out of an old movement
+                                       // and into a new movement. 
+
+    int update;                        // Variable to hold an intermediate value. If robot is accelerating/decelerating this
+                                       // is used to compare against new/old movements. 
+
+    int interruptPin = 2;              // Pin used to signal an obstacle has been detected by sonars. Logic High. 
+
+    void (*function[4])(uint8_t) = {&rotateRight, &rotateLeft, &goForward, &goBackward};  // An array of pointers to movement functions. 
+
+void setup() {
+  pinMode(interruptPin, INPUT);  // Set Mega pin 2 to input. 
+  roboclaw.begin(2400);          // Baud rate of connection to motor driver
+  pinMode(22, OUTPUT);           // not sure? 
+  Serial.begin(9600);            // serial to pc
+  Wire.begin();
+
+  // instantiate PID constants
+  roboclaw.SetM1Constants(0x80,Kd,Kp,Ki,qpps);
+  roboclaw.SetM2Constants(0x80,Kd,Kp,Ki,qpps); 
+  roboclaw.SetM1Constants(0x81,Kd,Kp,Ki,qpps);
+  roboclaw.SetM2Constants(0x81,Kd,Kp,Ki,qpps);  
+}
+
+///////////////       Main Loop        ///////////////////
+
+void loop() { 
+
+    if(Serial.available()){                        // If new movement available, retreive.
+    new_movement = Serial.read();      
+    }
+
+    was_interrupt = digitalRead(interruptPin);    // Poll interrupt pin from sonar controller. 
+   if(was_interrupt == true){
+     Serial.println("Interrupt true.");
+     stopmoving = true;                           // If pin HIGH then set stopmoving to true. 
+     detected_obstacle();                         // jump to stop routine for detected obstacle
+     was_interrupt = false;
+    }
+
+    switch(new_movement){                         // switch case for debugging using keyboard controls
+
+      case 108:                  // Keyboard l      Right
+        new_movement = RIGHT;
+        stopmoving = false;                         // Left and Right are reversed for Kinect Control. Because
+        break;                                      // the control happens while facing the robot
+
+      case 106:                  // Keyboard j     i Left
+        new_movement = LEFT;
+        stopmoving = false;
+        break;
+
+      case 107:
+        new_movement = STOP;        // Keyboard k      Stop
+        //detected_obstacle();
+        //new_movement = old_movement;
+        stopmoving = true;
+        new_movement = Stop_old_movement();
+        break;
+
+      case 105:                  // Keyboard i      Forward
+        new_movement = FORWARD;
+        stopmoving = false;
+        break;  
+
+      case 44:                   // Keyboard ,      Backward
+        new_movement = BACKWARD;
+        stopmoving = false;
+        break;
+
+        default: 
+        break;
+    }
+
+    doMove(new_movement);
+    /*if(stopmoving == false && new_movement < available_movements){        // If robot was not instructed to stop 
+       new_movement = Stop_old_movement();                                // and new_movement is a valid movement
+                                                                          // deccelerate from old movement. 
+
+       if(stopmoving == false && new_movement < available_movements){     // If robot was not instructed to stop
+         first_iteration = true;                                          // and new_movement is a valid movement
+         new_movement = Start_new_movement();                             // set first_iteration to true, signaling
+       }                                                                  // the first time in the acceleration loop. 
+    }*/
+
+}
+/////////////////       End Main Loop            /////////////////////////////
+
+void doMove(int new_move) {
+ if(stopmoving == false && new_movement < available_movements){        // If robot was not instructed to stop 
+       new_movement = Stop_old_movement();                                // and new_movement is a valid movement
+                                                                          // deccelerate from old movement. 
+
+       if(stopmoving == false && new_movement < available_movements){     // If robot was not instructed to stop
+         first_iteration = true;                                          // and new_movement is a valid movement
+         new_movement = Start_new_movement();                             // set first_iteration to true, signaling
+       }                                                                  // the first time in the acceleration loop. 
+    } 
+}
+
+/////////////////       Stop Old Movement Function       /////////////////////
+/*
+When we jump into this loop we set update to the value of new_movement. This 
+variable allows us to make sure that if we are sent a new movement during this 
+loop we have a variable to compare to. We then deccelerate from the old movement
+until we reach a speed of 0. At which point we update the old_movement to the 
+new_movement. 
+
+If we enter this loop and we have been told that to stop (stopmoving = true)
+then new_movement becomes equal to old_movement.
+
+We continually check to make sure we are not receiving new movement data. 
+If we do receive new movement data we jump out of Stop_old_movement into the 
+main loop.
+
+We always send back new_movement to the main loop in case it has changed. 
+*/
+int Stop_old_movement(){ 
+  update = new_movement;                                   
+          while( new_movement != old_movement && MotorSpeed > NoMotorSpeed){
+              Serial.println("stopping old movement ...");
+              MotorSpeed = MotorSpeed - increment;
+              first_iteration = false;
+              function[old_movement](MotorSpeed);
+
+              if (MotorSpeed == NoMotorSpeed && stopmoving == true){
+                new_movement = old_movement;
+                Serial.println("updated kinect value from STOP");
+              }
+               else if (MotorSpeed == NoMotorSpeed){
+              old_movement = new_movement;
+              Serial.println("updated kinect value from STOP");
+              }
+
+              if(Serial.available()){
+                 update = Serial.read();
+                 if (update != new_movement && update != 10){
+                 new_movement = update;
+                 Serial.println(update);
+                 Serial.println("Interrupted Stop Movement"); 
+                 break;
+                 }
+              }
+          }
+return new_movement;
+}
+/////////////////       End stop Old Movement Function       /////////////////////
+
+/////////////////       Start New Movement Function       /////////////////////
+/*
+Similar to the stop function the start continually checks for new movement input. 
+It accelerates up to the TopMotorSpeed and leaves the loop. first_iteration is 
+set to false after the first call of a movement function so that the Mega does not
+continue sending movement data to the sonar controller when the movement isn't changing. 
+*/
+
+int Start_new_movement(){
+   update = new_movement;
+      for( MotorSpeed; MotorSpeed <= TopMotorSpeed; MotorSpeed = MotorSpeed + increment){
+                function[new_movement](MotorSpeed);
+                first_iteration = false;
+                if(MotorSpeed == TopMotorSpeed){
+                old_movement = new_movement; 
+                Serial.println("updated kinect from START");
+               }
+
+               if(Serial.available()){
+                 update = Serial.read();
+                 if (update != new_movement && update != 10){
+                 old_movement = new_movement;
+                 new_movement = update;
+                 Serial.println(update);
+                 Serial.println("Interrupted Start Movement"); 
+                 break;
+                 } 
+                }
+              }
+       
+  return new_movement;
+}
+
+void rotateRight(uint8_t MotorSpeed){
+  Serial.print("Rotate Right:   ");
+  Serial.println(MotorSpeed);
+  roboclaw.BackwardM1(0x80, MotorSpeed);
+  roboclaw.ForwardM2(0x80,MotorSpeed);
+  roboclaw.BackwardM1(0x81,MotorSpeed);
+  roboclaw.ForwardM2(0x81,MotorSpeed);
+  if(first_iteration == true){
+    Serial.println("    First Time Right");
+    Wire.beginTransmission(sonar_controller);
+    Wire.write(new_movement);
+    Wire.endTransmission();
+  }
+   was_interrupt = digitalRead(interruptPin);
+   if(was_interrupt == true){
+     stopmoving = true; 
+     detected_obstacle();
+    }
+
+}
+
+void rotateLeft(uint8_t MotorSpeed){
+  Serial.print("Rotate Left:   ");
+  Serial.println(MotorSpeed);
+  roboclaw.ForwardM1(0x80, MotorSpeed);
+  roboclaw.BackwardM2(0x80,MotorSpeed);
+  roboclaw.ForwardM1(0x81,MotorSpeed);
+  roboclaw.BackwardM2(0x81,MotorSpeed);
+  if(first_iteration == true){
+    Serial.println("    First Time Left");
+    Wire.beginTransmission(sonar_controller);
+    Wire.write(new_movement);
+    Wire.endTransmission();
+  }
+
+      was_interrupt = digitalRead(interruptPin);
+    if(was_interrupt == true){
+       stopmoving = true; 
+       detected_obstacle();
+    }
+}
+
+void goForward(uint8_t MotorSpeed){
+  Serial.print("Forward:   ");
+  Serial.println(MotorSpeed);
+  roboclaw.ForwardM1(0x80,MotorSpeed);
+  roboclaw.ForwardM2(0x80,MotorSpeed);
+  //roboclaw.ForwardM2(0x80,15);
+  roboclaw.ForwardM1(0x81,MotorSpeed);
+  roboclaw.ForwardM2(0x81,MotorSpeed);
+  //roboclaw.ForwardM2(0x81,15);
+  if(first_iteration == true){
+    Serial.println("   First Time Forward");
+    Wire.beginTransmission(sonar_controller);
+    Wire.write(new_movement);
+    Wire.endTransmission();
+  }
+
+      was_interrupt = digitalRead(interruptPin);
+    if(was_interrupt == true){
+       stopmoving = true;
+       detected_obstacle();
+    }
+}
+
+void goBackward(uint8_t MotorSpeed){
+  Serial.print("Backward:   ");
+  Serial.println(MotorSpeed);
+  roboclaw.BackwardM1(0x80,MotorSpeed);
+  roboclaw.BackwardM2(0x80,MotorSpeed);
+  roboclaw.BackwardM1(0x81,MotorSpeed);
+  roboclaw.BackwardM2(0x81,MotorSpeed);
+  if(first_iteration == true){
+    Serial.print("   First Backward");
+    Wire.beginTransmission(sonar_controller);
+    Wire.write(new_movement);
+    Wire.endTransmission();
+  }
+
+    was_interrupt = digitalRead(interruptPin);
+    if(was_interrupt == true){
+      stopmoving = true; 
+      detected_obstacle();
+
+    }
+}
+
+void goStop(){
+  roboclaw.ForwardM1(0x80,0);
+  roboclaw.ForwardM2(0x80,0);
+  roboclaw.ForwardM1(0x81,0);
+  roboclaw.ForwardM2(0x81,0);
+  new_movement = old_movement;
+  MotorSpeed = 0;
+}
+
+// Detected Obstacle Function //
+/* 
+This function runs when the sonar controller sets 
+the Mega pin 2 HIGH. Indicated that an obstacle is 
+in the way of the robot. The Uno will send which 
+sonar detected the obstacle and how far the obstacle 
+is from that sonar. If the obstacle is less than 10 inches
+away it will perform an emergency stop where the robot
+stops immediately. If 10inches < obstacle < 15inches the
+the robot will decelerate. This function will get stuck
+in the while loop if the obstacle remains, thereby 
+preventing the robot from moving until the obstacle
+has been removed. 
+*/
+
+void detected_obstacle(){
+  uint8_t sonar_number, obstacle;
+  
+  //char sonar_number, obstacle;
+  Wire.requestFrom(sonar_controller, 2);
+  int i = 0;
+  delay(100);
+  Serial.print("Bytes available: ");
+  Serial.println(Wire.available());
+  /*while(Wire.available())
+  {
+    byte c = Wire.read();
+    Serial.print("From wire: ");
+    Serial.println(c);
+    //uno[i] = c;
+    //obstacle = Wire.read();    
+    //Serial.print(" obstacle:    ");
+    //Serial.print(obstacle);
+    //i = i+1;
+  }*/
+  /*if(Wire.available())
+  {
+    sonar_number = Wire.read();
+    //Serial.print("From wire: ");
+    //Serial.println(c); 
+    Serial.print(" sonar number:  "); 
+    Serial.println(sonar_number);
+  }*/
+   delay(100);
+   obstacle = Wire.read();
+   delay(100);
+   sonar_number = Wire.read();
+   Serial.print(" obstacle:    ");
+   Serial.print(obstacle);
+   Serial.print(" sonar number:  "); 
+   Serial.println(sonar_number);
+    if(obstacle < 10){
+     Serial.println("obstacle < 10"); 
+      //goStop();
+      new_movement = STOP;
+      doMove(new_movement);
+      //decide(obstacle, sonar_number);
+      
+      } else{
+        new_movement = Stop_old_movement();
+      }
+   //delayMicroseconds(250);
+   int count=0;
+   while((obstacle < threshold) && (sonar_number > 0)){
+     Serial.println("waiting until no more obstacle");
+     Wire.requestFrom(sonar_controller, 2);
+    
+     obstacle = Wire.read();
+     sonar_number = Wire.read();
+     Serial.write(sonar_number);
+     //Serial.write("  ");
+     //Serial.write(obstacle);
+     delayMicroseconds(250);
+     decide(obstacle, sonar_number);
+     
+     if (count>100) {
+       count = 0;
+       break;
+     } else {
+       count = count + 1;
+       Serial.println(count);
+     }
+   }
+   Serial.println("end detected_obstacle()");
+   stopmoving = false;
+
+}
+
+// End Detected Obstacle Function // 
+
+// Decide what to do / where to move when there's an obstacle
+void decide(int obstacle, int sonar_number) {
+  // Sonars:
+  // front left: 1,2,3
+  // back left:  4,5,6
+  // back right: 7,8,9
+  // front right: 10,11,12
+  Serial.print("Obstacle: ");
+  Serial.println(obstacle);
+  Serial.print("Sonar number: ");
+  Serial.println(sonar_number);
+  Serial.println("Trying to decide what to do...");
+  if (sonar_number==7||sonar_number==6||sonar_number==5) {
+    Serial.println("Turn Right");
+    new_movement=RIGHT;
+    //doMove();  // turn right
+  } else if (sonar_number==4||sonar_number==8) {
+    Serial.println("Go Forward");
+    new_movement=FORWARD;
+    //doMove(FORWARD);  // go forward
+  } else if (sonar_number==12) {
+    Serial.println("Turn Left");
+    new_movement=LEFT;
+    //doMove(LEFT);  // turn left
+  } else if (sonar_number==13||sonar_number==7) {
+    Serial.println("Go Reverse");
+    new_movement=BACKWARD;
+    //doMove(BACKWARD);  // go reverse
+  } else {
+    Serial.println("Stopping...");
+    new_movement=STOP;
+    //doMove(STOP);
+  }
+  doMove(new_movement);
+}
+///////
+//
+void displayspeed(void)
+{
+  uint8_t status;
+  bool valid;
+
+  uint32_t enc1 = roboclaw.ReadEncM1(0x80, &status, &valid);
+  if(valid){
+    Serial.print("Encoder1:");
+    Serial.print(enc1,DEC);
+    Serial.print(" ");
+    Serial.print(status,HEX);
+    Serial.print(" ");
+  }
+  uint32_t enc2 = roboclaw.ReadEncM1(0x81, &status, &valid);
+  if(valid){
+    Serial.print("Encoder2:");
+    Serial.print(enc2,DEC);
+    Serial.print(" ");
+    Serial.print(status,HEX);
+    Serial.print(" ");
+  }
+  uint32_t speed1 = roboclaw.ReadSpeedM1(0x80, &status, &valid);
+  if(valid){
+    Serial.print("Speed1:");
+    Serial.print(speed1,DEC);
+    Serial.print(" ");
+  }
+  uint32_t speed2 = roboclaw.ReadSpeedM1(0x81, &status, &valid);
+  if(valid){
+    Serial.print("Speed2:");
+    Serial.print(speed2,DEC);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
---
+++ src/navigation/nav_sensing/mcecsbot_sonar_with_uint8_2/mcecsbot_sonar_with_uint8_2.ino
@@ -0,0 +1,305 @@
+// Slave Sonar
+// Jesse Adams 
+#include <Wire.h>
+#define arraysize 5 // size of array that stores sonar data
+
+uint8_t pulse1;
+uint8_t pulse2;
+uint8_t pulse3;
+uint8_t pulse4;
+uint8_t pulse5;
+uint8_t pulse6;
+uint8_t pulse7;
+uint8_t pulse8;
+uint8_t pulse9;
+uint8_t pulse10;
+uint8_t pulse11;
+uint8_t pulse12;
+
+uint8_t sonar1 = 7;
+uint8_t sonar2 = 6;
+uint8_t sonar3 = 5;
+uint8_t sonar4 = 2;
+uint8_t sonar5 = 3;
+uint8_t sonar6 = 4;
+uint8_t sonar7 = 10;
+uint8_t sonar8 = 9;
+uint8_t sonar9 = 8;
+uint8_t sonar10 = 11;
+uint8_t sonar11 = 12;
+uint8_t sonar12 = 13;
+int InterruptPin = A1;
+
+boolean ALERT = false;          // Tells whether object detect. Must "alert" the mega
+uint8_t obstacle;               // Holds value for data transmission to mega
+uint8_t data_to_send[2];        // In I2C protocol, if sending multiple bytes, they 
+                                // must be stored in a uint8_t array
+int threshold = 15;             // distance in inches that will trigger robot to stop
+int kinectVal = -1;             // initializing value. ie don't do anything until instructed
+int rangevalue[arraysize] = { 0, 0, 0, 0, 0 };  // data array for sonars
+int slave_address = 2;          // I2C slave address of Arduino
+
+void setup()
+{
+  Wire.begin(slave_address);      // join i2c bus with address #2
+  Wire.onReceive(receiveEvent);   // register event
+  Wire.onRequest(requestEvent);   // register event
+  Serial.begin(9600);             // start serial for output
+  pinMode(InterruptPin, OUTPUT);  // Alert pin for Mega
+  digitalWrite(InterruptPin, ALERT);  // Initialize as low 
+
+  delay(250);                    // delay required for sonars
+
+  pulseIn(sonar1, HIGH);         // Each sonar must be initialized with a read of 102ms
+  delay(102);                    // this is a calibration reading
+  pulseIn(sonar2, HIGH);
+  delay(102);
+  pulseIn(sonar3, HIGH);
+  delay(102);
+  pulseIn(sonar4, HIGH);
+  delay(102);
+  pulseIn(sonar5, HIGH);
+  delay(102);
+  pulseIn(sonar6, HIGH);
+  delay(102);
+  pulseIn(sonar7, HIGH);
+  delay(102);
+  pulseIn(sonar8, HIGH);
+  delay(102);
+  pulseIn(sonar9, HIGH);
+  delay(102);
+  pulseIn(sonar10, HIGH);
+  delay(102);
+  pulseIn(sonar11, HIGH);
+  delay(102);
+  pulseIn(sonar12, HIGH);
+  delay(102); 
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ // Code that executes while waiting from I2C communication
+  /*if(Serial.available()){                        // If new movement available, retreive.
+    kinectVal = Serial.read();   
+   Serial.print("Kinectval: ");
+   Serial.println(kinectVal);
+    }*/
+    pulse12 = getSonar(sonar12);
+        if(pulse12 < threshold){
+           stop_robot(sonar12, pulse12);
+        }
+        
+    
+  switch(kinectVal){
+
+    case 0:                              // case 0: Turn Left 
+    Serial.println("Case 0");
+      pulse3 = getSonar(sonar3);
+        if(pulse3 < threshold){
+           stop_robot(sonar3, pulse3);
+        }
+
+      pulse5 = getSonar(sonar5);
+        if(pulse5 < threshold){
+           stop_robot(sonar5, pulse5);
+         }
+
+      pulse8 = getSonar(sonar8);
+        if(pulse8 < threshold){
+           stop_robot(sonar8, pulse8);
+         }
+      pulse11 = getSonar(sonar11);
+        if(pulse11 < threshold){
+           stop_robot(sonar11, pulse11);
+         }
+
+    break;
+
+    case 1:                              // case 1: Turn Right
+    Serial.println("Case 1");
+      pulse3 = getSonar(sonar3);
+        if(pulse3 < threshold){
+           stop_robot(sonar3, pulse3);
+        }
+
+      pulse5 = getSonar(sonar5);
+        if(pulse5 < threshold){
+           stop_robot(sonar5, pulse5);
+         }
+
+      pulse8 = getSonar(sonar8);
+        if(pulse8 < threshold){
+           stop_robot(sonar8, pulse8);
+         }
+      pulse11 = getSonar(sonar11);
+        if(pulse11 < threshold){
+           stop_robot(sonar11, pulse11);
+         }
+    break;
+
+    case 2:                              // case 2: Move Forward 
+    Serial.println("Case 2");
+      pulse1 = getSonar(sonar1);
+        if(pulse1 < threshold){
+           stop_robot(sonar1, pulse1);
+        }
+      pulse2 = getSonar(sonar2);   
+        if(pulse2 < threshold){
+           stop_robot(sonar2, pulse2);
+        }
+      pulse11 = getSonar(sonar11);
+        if(pulse11 < threshold){
+           stop_robot(sonar11, pulse11);
+        }
+      pulse12 = getSonar(sonar12);   
+        if(pulse12 < threshold){
+           stop_robot(sonar12, pulse12);
+        }
+
+    break;
+
+    case 3:                              // case 3: Move Backward 
+    Serial.println("Case 3");
+      pulse6 = getSonar(sonar6);
+           if(pulse6 < threshold){
+           stop_robot(sonar6, pulse6);
+        }
+      pulse7 = getSonar(sonar7);
+           if(pulse7 < threshold){
+           stop_robot(sonar7, pulse7);
+        }
+      pulse8 = getSonar(sonar8);
+           if(pulse8 < threshold){
+           stop_robot(sonar8, pulse8);
+        }
+      pulse9 = getSonar(sonar9);
+           if(pulse9 < threshold){
+           stop_robot(sonar9, pulse9);
+        }
+    break;
+
+    case 4:
+   // Stopped
+   Serial.println("Case 4");
+    break;
+
+  }
+}
+
+///             Get Sonar Function              ///
+/* Programmer passes sonar number into this function
+to return sonar reading. i.e. getSonar(sonar3);   */
+
+int getSonar(int sonar) {
+      int pulse;
+      for(int i = 0 ; i < arraysize; i ++){
+          pulse = pulseIn(sonar, HIGH);
+          delay(48);
+          rangevalue[i] = pulse/147;
+      }
+      isort(rangevalue, arraysize);
+      pulse = mode(rangevalue, arraysize);
+    return pulse;
+}
+
+///////////////   end Sonar Function  ///////////////
+
+///             Stop Robot Function              ///
+/* If any sonar detects a reading of less than the 
+variable "threshold" it will call this function. 
+Starts by setting the Interrupt Pin High to noticy 
+the Mega. Then it spins in a while loop, continuously
+reading the sonar that detected the obstacle. Once 
+the obstacle is removed the loop ends and the function
+removes the alert to the Mega by driving the interrupt
+pin low. */
+
+void stop_robot(uint8_t sonar, uint8_t obstacle){
+  ALERT = HIGH;
+  data_to_send[0] = obstacle;
+  data_to_send[1] = sonar;
+  digitalWrite(InterruptPin, ALERT);
+    while(obstacle < threshold){
+      data_to_send[0] = obstacle;
+      data_to_send[1] = sonar;
+      Serial.print("obstacle: ");
+      Serial.print(obstacle);
+      Serial.print("  sonar: ");
+      Serial.print(sonar);            // Debug Block
+      Serial.print("  ALERT: ");
+      Serial.println(ALERT);
+      Serial.print("Data_to_send0: ");
+        Serial.println(data_to_send[0]);
+        Serial.print("Data_to_send1: ");
+        Serial.println(data_to_send[1]);
+      obstacle = getSonar(sonar);
+    }
+   ALERT = LOW;
+   digitalWrite(InterruptPin, ALERT);
+   data_to_send[0] = 25;
+   data_to_send[1] = 25;
+}  
+
+// function that executes whenever data is requested from master
+// this function is registered as an event, see setup()   
+void requestEvent()
+{
+  Wire.write(data_to_send, 2);  // respond with message of 2 bytes
+                                // as expected by master
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  int kinectVal = Wire.read();        // receive byte as an integer
+//  Serial.println(kinectVal);          // print the integer
+}
+
+// isort is used to sort the array of read values from a sonar //
+void isort(int *a, int n){
+  for (int i = 1; i < n; ++i){                    
+    int j = a[i];                                 
+    int k;                          
+    for (k = i - 1; (k >= 0) && (j < a[k]); k--){
+      a[k + 1] = a[k];
+      }
+    a[k + 1] = j;
+  }
+}
+//////////////////////     End isort        ////////////////////
+
+///        Mode function, returns the mode or median.        ///
+int mode(int *x,int n){
+  int i = 0;
+  int count = 0;
+  int maxCount = 0;
+  int mode = 0;
+  int bimodal;
+  int prevCount = 0;
+  while(i<(n-1)){
+    prevCount=count;
+    count=0;
+    while(x[i] == x[i+1]){
+      count++;
+      i++;
+    }
+    if(count>prevCount&count>maxCount){
+      mode=x[i];
+      maxCount=count;
+      bimodal=0;
+    }
+    if(count==0){
+      i++;
+    }
+    if(count==maxCount){      //If the dataset has 2 or more modes.
+      bimodal=1;
+    }
+    if(mode==0||bimodal==1){//Return the median if there is no mode.
+      mode=x[(n/2)];
+    }
+    return mode;
+  }
+}
+////////////////////         end mode       /////////////////////// 
