5e695f6131c8b188f17ceba6e7038e2c77677d5b
Mathias Sunardi <m.sunardi@gmail.com>
2013-08-09 11:13:14 -0700

    Server can now receive and broadcast JSON messages

--- src/subsystem_communications/server.py
+++ src/subsystem_communications/server.py
@@ -14,52 +14,67 @@ class IphoneChat(Protocol):
 		self.factory.clients.remove(self)
 
 	def dataReceived(self, data):
-		a = data.split(':')
-		print a
-		if len(a) > 1:			
-			if len(a) == 3:
-				sender = a[0]
-				target = a[1].rstrip()
-				command = a[2].rstrip()
-				
-				#if target == "body":
-				#	msg = target + ":" + command
-				#	print msg
-				#msg = target + ":" + command
-				msg = sender + ":" + target + ":" + command
-				print msg
-					
-				#for c in self.factory.clients:
-				#	c.message(msg)
-
-			elif len(a) == 2:
-				command = a[0]
-				content = a[1].rstrip()
-				content = content.lstrip()
-				print command
-				print content
-				msg = ""
-				if command == "iam":
-					self.name = content
-					msg = self.name + " has joined."
-					print msg
-	
-				elif command == "msg":
-					msg = self.name + ": " + content
-					print msg
-					#for c in self.factory.clients:
-					#	log.msg("Client connection from %s" % c)
-				else:
-					print "I don't understand."	
-                        else:
-                        	msg = data
-	
+		json = False
+		a = data.split('!')
+		print a[0]
+		if a[0]=="json":
+			print "json true"
+			json = True
+			msg = a[1:]
+			print "json: "
+			print msg[0]
 		else:
-			print "I don't understand!"
-			msg = "I don't understand what you're trying to do."
+			a = data.split(':')
+			print "data: "
+			print a
+			if (len(a) > 1) and not json:			
+				if len(a) == 3:
+					sender = a[0]
+					target = a[1].rstrip()
+					command = a[2].rstrip()
+					
+					#if target == "body":
+						#	msg = target + ":" + command
+						#	print msg
+					#msg = target + ":" + command
+					msg = sender + ":" + target + ":" + command
+					print msg
+					
+					#for c in self.factory.clients:
+					#	c.message(msg)
+
+				elif len(a) == 2:
+					command = a[0]
+					content = a[1].rstrip()
+					content = content.lstrip()
+					print command
+					print content
+					msg = ""
+					if command == "iam":
+						self.name = content
+						msg = self.name + " has joined."
+						print msg
+	
+					elif command == "msg":
+						msg = self.name + ": " + content
+						print msg
+						#for c in self.factory.clients:
+						#	log.msg("Client connection from %s" % c)
+					else:
+						print "I don't understand."	
+                        	else:
+                        		msg = data
+	
+			else:
+				print "I don't understand!"
+				#msg = "I don't understand what you're trying to do."
 		
+		print "messaging...\n"	
 		for c in self.factory.clients:
-			c.message(msg)
+			if not json:
+				c.message(msg)
+			else:
+				c.message(msg[0])
 			
 
 	def message(self, message):
