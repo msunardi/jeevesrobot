c7d0f7373b4ab535571a9e82446ba0f1e0fd7635
Mathias Sunardi <msunardi@pdx.edu>
2014-03-18 16:13:33 -0700

    jeeves_tests.launch now includes roboteq_controllers.launch. Adding files to enable remote connections to rosmaster from another machine.

---
+++ src/ros/launch/client_hostname_exports.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Edit this file, then source it on any client machine that needs
+# communication with the ROS master on jeeves.
+
+# IP of the remote client.
+export ROS_IP=131.252.201.217
+
+# Hostname of the remote client
+# (or simply the IP, if you don't have name resolution).
+export ROS_HOSTNAME=131.252.201.217
+
+# URI of the robot
+export ROS_MASTER_URI=131.252.224.44:11311
---
+++ src/ros/launch/jeeves_hostname_exports.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# source this file before running roscore or a roslaunch file on jeeves
+# if you want remote clients to connect to the ros master on jeeves.
+
+export ROS_IP=131.252.224.44
+export ROS_HOSTNAME=131.252.224.44
+export ROS_MASTER_URI=131.252.224.44:11311
