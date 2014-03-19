54fafd818a4a65a4da2692213d308bda7b498fa8
Mathias Sunardi <msunardi@pdx.edu>
2014-03-19 12:40:26 -0700

    Using jeeves.dnsdynamic.com for our hostname exports.

--- src/ros/launch/jeeves_hostname_exports.bash
+++ src/ros/launch/jeeves_hostname_exports.bash
@@ -2,6 +2,6 @@
 # source this file before running roscore or a roslaunch file on jeeves
 # if you want remote clients to connect to the ros master on jeeves.
 
-export ROS_IP=131.252.224.44
-export ROS_HOSTNAME=131.252.224.44
-export ROS_MASTER_URI=http://131.252.224.44:11311
+export ROS_IP=jeeves.dnsdynamic.com
+export ROS_HOSTNAME=jeeves.dnsdynamic.com
+export ROS_MASTER_URI=http://jeeves.dnsdynamic.com:11311
