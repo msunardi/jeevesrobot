8b6d37fd01003b1ef95add91bc28c183a489b3ce
Rick Armstrong <richarda@pdx.edu>
2014-10-11 17:28:17 -0700

    We now set appropriate ROS_IP, ROS_MASTER_URI, and ROS_HOSTNAME in jeeves.launch. Also, removing now-superfluous jeeves_fused_odometry.launch and start_jeeves.bash.

--- src/ros/launch/jeeves_hostname_exports.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# source this file before running roscore or a roslaunch file on jeeves
-# if you want remote clients to connect to the ros master on jeeves.
-
-export ROS_IP=192.168.1.2
-export ROS_HOSTNAME=192.168.1.2
-export ROS_MASTER_URI=http://192.168.1.2:11311
--- src/ros/launch/start_jeeves.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source ./jeeves_hostname_exports.bash
-roslaunch ./jeeves_fused_odometry.launch
-
