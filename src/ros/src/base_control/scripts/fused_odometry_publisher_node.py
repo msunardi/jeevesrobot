7a46bd870efba2578cb9ef76800d926262258bad
Rick Armstrong <richarda@pdx.edu>
2014-10-19 16:03:43 -0700

    Re-instated median filtering of incoming wheel velocities. Now using laser_scan_matcher's pose_stamped message instead of generating timestamps at time of receipt.

--- src/ros/src/base_control/scripts/fused_odometry_publisher_node.py
+++ src/ros/src/base_control/scripts/fused_odometry_publisher_node.py
@@ -9,7 +9,7 @@ import time
 
 import roslib
 import rospy
-from geometry_msgs.msg import Twist, Point, Pose2D, Quaternion
+from geometry_msgs.msg import Twist, Point, Pose2D, PoseStamped, Quaternion
 from nav_msgs.msg import Odometry
 from roboteq_msgs.msg import Command as MotorCommand
 from roboteq_msgs.msg import Feedback as MotorFeedback
@@ -20,13 +20,10 @@ import tf
 from base_control_node import BaseTransformHandler, WHEEL_RADIUS_m, \
     HALF_WHEELBASE_X_m, HALF_WHEELBASE_Y_m
 
-# we get feeback messages from the motor controllers at 50 Hz,
-# so let's publish at the same rate
 ODOMETRY_UPDATE_RATE_Hz = 50
 
 class OdometryPublisher(threading.Thread):
     def __init__(self, transformer, odometry_update_rate_hz):
-
         # front motors are #1, #4
         self.motor_1_listener = rospy.Subscriber(
             "/motor_1/feedback",
@@ -50,20 +47,16 @@ class OdometryPublisher(threading.Thread):
             self.motor_3_feedback_callback)
 
         self.lsm_listener = rospy.Subscriber(
-            "/pose2D",
-            Pose2D,
+            "/pose_stamped",
+            PoseStamped,
             self.lsm_pose2D_callback)
 
-        # angular velocities, rads/s
-        self.w_1 = 0.0
-        self.w_2 = 0.0
-        self.w_3 = 0.0
-        self.w_4 = 0.0
-        self.w_1_lock = threading.Lock()
-        self.w_2_lock = threading.Lock()
-        self.w_3_lock = threading.Lock()
-        self.w_4_lock = threading.Lock()
-        
+        # last few wheel angular velocities, rads/s
+        self.w_1 = deque([0.0, 0.0, 0.0, 0.0])
+        self.w_2 = deque([0.0, 0.0, 0.0, 0.0])
+        self.w_3 = deque([0.0, 0.0, 0.0, 0.0])
+        self.w_4 = deque([0.0, 0.0, 0.0, 0.0])
+
         self.transformer = transformer
         self.sleeper = rospy.Rate(odometry_update_rate_hz)
         self.delta_t = 1.0 / odometry_update_rate_hz
@@ -74,42 +67,55 @@ class OdometryPublisher(threading.Thread):
         self.child_frame_id = '/base_footprint'
         self.x = 0.0
         self.y = 0.0
-        self.theta = (0.0, 0.0)         # theta, timestamp (seconds)
-        self.theta_prev = (0.0, 0.1)    # theta, timestamp (seconds)
+        self.theta = (0.0, 0.0)         # theta, timestamp
+        self.theta_prev = (0.0, -1.0)   # theta, timestamp 
         self.lsm_theta = 0.0
-        self.pose_lock = Lock()
+        self.theta_lock = Lock()
+        self.w_1_lock = Lock()
+        self.w_2_lock = Lock()
+        self.w_3_lock = Lock()
+        self.w_4_lock = Lock()
         threading.Thread.__init__(self)
 
     def run(self):
         loop_count = 0
-        w = [0.0, 0.0, 0.0, 0.0]
         while not rospy.is_shutdown():
             self.sleeper.sleep()
-
-            # get the latest incoming update
+            
+            # get the incoming wheel velocities and filter
+            w_1 = []
+            w_2 = []
+            w_3 = []
+            w_4 = []
             with self.w_1_lock:
-                w[0] = self.w_1
+                w_1 = self.w_1
             with self.w_2_lock:
-                w[1] = self.w_1
+                w_2 = self.w_2
             with self.w_3_lock:
-                w[2] = self.w_1
+                w_3 = self.w_3
             with self.w_4_lock:
-                w[3] = self.w_1
+                w_4 = self.w_4
+            w = [np.median(w_1), np.median(w_2), np.median(w_3), np.median(w_4)]
 
-            # we don't trust the angular part of the twist message, so use
-            # the info from laser_scan_matcher
+            # we don't trust the angular part of our odometry measurements, so we
+            # overwrite it with orientation from laser_scan_matcher's pose estimate
             twist = self.transformer.wheel_velocities_to_twist(w)
-            with self.pose_lock:
-                twist.angular.z =  (self.theta[0] - self.theta_prev[0]) / (self.theta[1] - self.theta_prev[1])
+            theta = ()
+            theta_prev = ()
+            with self.theta_lock:
+                theta = self.theta
+                theta_prev = self.theta_prev
+            theta_delta_t = theta[1] - theta_prev[1]
+            twist.angular.z =  (theta_prev[0] - theta[0]) / theta_delta_t
             rospy.logdebug("OdometryPublisher.run(): wheel velocities: " + str(w))
             rospy.logdebug("OdometryPublisher.run(): twist: " + str(twist))
 
             # calculate our new position using a
             # simple deterministic model
-            delta_x = ((twist.linear.x * np.cos(self.theta[0]) - twist.linear.y
-                        * np.sin(self.theta[0])) * self.delta_t)
-            delta_y = ((twist.linear.x * np.sin(self.theta[0]) + twist.linear.y
-                        * np.cos(self.theta[0])) * self.delta_t)
+            delta_x = ((twist.linear.x * np.cos(theta[0]) - twist.linear.y
+                        * np.sin(theta[0])) * self.delta_t)
+            delta_y = ((twist.linear.x * np.sin(theta[0]) + twist.linear.y
+                        * np.cos(theta[0])) * self.delta_t)
             self.x += delta_x
             self.y += delta_y
 
@@ -121,7 +127,7 @@ class OdometryPublisher(threading.Thread):
 
             msg.twist.twist = twist
             msg.pose.pose.position = Point(self.x, self.y, 0.0)
-            msg.pose.pose.orientation = Quaternion(*(kdl.Rotation.RPY(0.0, 0.0, self.theta[0]).GetQuaternion()))
+            msg.pose.pose.orientation = Quaternion(*(kdl.Rotation.RPY(0.0, 0.0, theta[0]).GetQuaternion()))
 
             pos = (msg.pose.pose.position.x,
                    msg.pose.pose.position.y,
@@ -142,25 +148,34 @@ class OdometryPublisher(threading.Thread):
 
     def motor_1_feedback_callback(self, feedback_msg):
         with self.w_1_lock:
-            self.w_1 = feedback_msg.measured_velocity
+            self.w_1.append(feedback_msg.measured_velocity)
+            self.w_1.popleft()
 
     def motor_2_feedback_callback(self, feedback_msg):
         with self.w_2_lock:
-            self.w_2 = feedback_msg.measured_velocity
+            self.w_2.append(feedback_msg.measured_velocity)
+            self.w_2.popleft()
 
     def motor_3_feedback_callback(self, feedback_msg):
         with self.w_3_lock:
-            self.w_3 = feedback_msg.measured_velocity
+            self.w_3.append(feedback_msg.measured_velocity)
+            self.w_3.popleft()
 
     def motor_4_feedback_callback(self, feedback_msg):
         with self.w_4_lock:
-            self.w_4 = feedback_msg.measured_velocity
+            self.w_4.append(feedback_msg.measured_velocity)
+            self.w_4.popleft()
 
     def lsm_pose2D_callback(self, pose_msg):
-        with self.pose_lock:
+        with self.theta_lock:
             self.theta_prev = self.theta
-            self.theta = (pose_msg.theta, rospy.get_time())
-
+            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([
+                pose_msg.pose.orientation.x,
+                pose_msg.pose.orientation.y,
+                pose_msg.pose.orientation.z,
+                pose_msg.pose.orientation.w])
+            self.theta = (yaw, pose_msg.header.stamp.to_sec())
+            
 def main(args):
     rospy.init_node('base_odometry', anonymous=True, log_level=rospy.INFO)
     bth = BaseTransformHandler(WHEEL_RADIUS_m,
