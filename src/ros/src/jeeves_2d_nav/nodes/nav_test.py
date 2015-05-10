62cf9ed43ccc01b46ad599da223aacfbe3d344a5
Rick Armstrong <richarda@pdx.edu>
2015-05-10 00:43:45 -0700

    nav_test_node now pulls first waypoint from manager and goes there.

--- src/ros/src/jeeves_2d_nav/nodes/nav_test.py
+++ src/ros/src/jeeves_2d_nav/nodes/nav_test.py
@@ -7,26 +7,50 @@
 """
 import sys
 import threading
+import yaml
 
 import rospy
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, Point, Quaternion
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 import jeeves_2d_nav
 from jeeves_2d_nav.srv import *
 
 class NavTest(threading.Thread):
     def __init__(self):
+        self.halt = False
         self.sleeper = rospy.Rate(1)
+        self.waypoints = {}
+        self.mbc = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         threading.Thread.__init__(self)
         self.get_waypoints = rospy.ServiceProxy(
             '/waypoint_manager/get_waypoints', jeeves_2d_nav.srv.GetWaypoints)
 
     def run(self):
+        # load waypoints
         rospy.loginfo("Waiting for /waypoint_manager/get_waypoints service.")
         rospy.wait_for_service('/waypoint_manager/get_waypoints')
-        while not rospy.is_shutdown():
-            rospy.loginfo(self.get_waypoints())
-            self.sleeper.sleep()
+        try:
+            self.waypoints = yaml.load(self.get_waypoints().waypoints)
+        except Exception as e:
+            rospy.logerr("Exception getting waypoints: " + str(e.args))
 
+        self.mbc.wait_for_server(rospy.Duration(60))
+        wp = self.waypoints[0]
+        name = wp['name']
+        x = wp['x']
+        y = wp['y']
+        theta = wp['theta']
+        goal = MoveBaseGoal()
+        goal.target_pose.pose = Pose(Point(x, y, 0.0),
+                                     Quaternion(0.0, 0.0, 0.0, 1.0))
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        self.mbc.send_goal(goal)
+        result = self.mbc.wait_for_result(rospy.Duration(300))
+        rospy.loginfo("wait_for_result returned: " + str(result))
 
 def main(args):
     rospy.init_node('nav_test_node', anonymous=True)
