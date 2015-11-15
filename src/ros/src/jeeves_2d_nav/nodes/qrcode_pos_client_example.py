2cd0e22c042f992d427873d279e284378c1ab95b
Josh Sackos <contact@joshsackos.com>
2015-11-14 23:12:11 -0700

    Changed timeout to 30 sec.

--- src/ros/src/jeeves_2d_nav/nodes/qrcode_pos_client_example.py
+++ src/ros/src/jeeves_2d_nav/nodes/qrcode_pos_client_example.py
@@ -19,7 +19,7 @@ def get_position():
    rospy.wait_for_service('qrcode_pos_srv')
 
    qrcode_pos_h = rospy.ServiceProxy('qrcode_pos_srv', qrcode_pos)              # Set handler to service
-   resp_data = json.loads(qrcode_pos_h().json_resp)
+   resp_data = json.loads(qrcode_pos_h().json_resp)				# Make request to "qrcode_pos_srv", and decode JSON response string
    return resp_data                                                             # Return positional data received from service
 
 
@@ -29,7 +29,9 @@ def get_position():
 if __name__ == "__main__":
    print "Requesting position..."
    position = get_position()
+   # If the "valid" dictionary entry is True then valid data resides in the dictionary
    if position['valid']:
       print "x = %d, y = %d, z = %d, theta = %d" % (position["x"], position["y"], position["z"], position["theta"])
+   # The "valid" dictionary entry was False. This means an attempt to acquire position data from a QR code meant for the project failed.
    else:
       print "Invalid coordinates received..."
