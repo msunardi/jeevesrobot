d40c3c27bcef9c6da9fd03626d4bfffb60437346
Josh Sackos <contact@joshsackos.com>
2015-11-14 22:55:39 -0700

    Updated QR code tool

--- src/ros/src/jeeves_2d_nav/nodes/qrcode_pos_service.py
+++ src/ros/src/jeeves_2d_nav/nodes/qrcode_pos_service.py
@@ -10,9 +10,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-#from qrtools import QR
 import zbar
-#import Image
 import json
 
 # ---------------------------------------------------------------------
@@ -37,11 +35,11 @@ def get_position_h(req):
 
       # If timeout reached return exception
       sec_cnt += 1
-      if sec_cnt == sec_cnt_timeout:
+      if sec_cnt == SEC_CNT_TIMEOUT:
          bln_timeout = True
       
       # Get a single image from freenect Kinect node
-      image = rospy.wait_for_message("/camera/rgb/image_color", Image, timeout=sec_cnt_timeout)
+      image = rospy.wait_for_message("/camera/rgb/image_color", Image, timeout=SEC_CNT_TIMEOUT)
 
       # Decode QR code if exists
       if image:
@@ -82,7 +80,7 @@ def proc_image(ros_image):
    # Convert ROS RGB image to OpenCV 8-bit grayscale image
    cv_image = CvBridge().imgmsg_to_cv2(ros_image, "mono8")
    # Extract only the actual image data and pack as binary data
-   string_data = struct.pack('<%dc' % 307200, *cv_image.data)
+   string_data = struct.pack('<%dc' % (img_width*img_height), *cv_image.data)
 
    # Create a Zbar scanner
    scanner = zbar.ImageScanner()
@@ -110,23 +108,26 @@ def proc_image(ros_image):
    if data:
      print data
      
-     split_data = data.split(",")
-     x = int(split_data[0].replace('x=', ''))
-     y = int(split_data[1].replace('y=', ''))
-     z = int(split_data[2].replace('z=', ''))
-     theta = int(split_data[3].replace('theta=', ''))
-   
-     # Return success
-     return [True,json.dumps({"valid":True,"x":x,"y":y,"z":z,"theta":theta})]
+     # Make sure the QR code was meant for use with the project specified
+     if data.find('project=%s' % str(PROJECT_VALUE)) != -1:
+     
+        split_data = data.split(",")
+        x = int(split_data[0].replace('x=', ''))
+        y = int(split_data[1].replace('y=', ''))
+        z = int(split_data[2].replace('z=', ''))
+        theta = int(split_data[3].replace('theta=', ''))
+      
+        # Return success
+        return [True,json.dumps({"valid":True,"x":x,"y":y,"z":z,"theta":theta})]
    
    # Could not successfully find/decode a QR code in image
-   return [False,json.dumps({"valid":False,"x":-1,"y":-1,"z":-1,"theta":-1})]
+   return [False,json.dumps({"valid":False})]
 
 # ---------------------------------------------------------------------
 #                        qrcode_pos_service()
 # ---------------------------------------------------------------------
 def qrcode_pos_service():
-#   rospy.Subscriber("/camera/rgb/image_color", Image, proc_image)          # Subscribe to the 'chatter' topic, creates a new thread.
+#   rospy.Subscriber("/camera/rgb/image_color", Image, proc_image)         # Subscribe to the 'chatter' topic, creates a new thread.
    s = rospy.Service('qrcode_pos_srv', qrcode_pos, get_position_h)         # Start the 'qrcode_pos_srv' service, of type 'scripts/qrcode_pos.srv', with handler function 'get_position_h'
    print "Ready to accept position requests..."
    rospy.spin()                                                            # Prevents script from terminating until service stops
@@ -136,8 +137,9 @@ def qrcode_pos_service():
 # ---------------------------------------------------------------------
 if __name__ == "__main__":
    rospy.init_node('qrcode_pos')                                           # Initialize ROS node
-   sec_cnt_timeout = rospy.get_param('~timeout_value')                         # Number of seconds before timeout
-   qrcode_pos_service()
+   SEC_CNT_TIMEOUT = rospy.get_param('~timeout_value')                     # Number of seconds before timeout
+   PROJECT_VALUE = rospy.get_param('~project')                             # Project that this service should look for in QR codes found
+   qrcode_pos_service()                                                    # Start the qrcode_pos_srv service
    
    
    
