b531a0e5f6ab649ed789d15ff00bd2fb26c9e19a
Rick Armstrong <richarda@pdx.edu>
2016-03-15 03:05:31 -0700

    Add link to navigation app

--- src/ros/src/web_ui/server/navigation/urls.py
+++ src/ros/src/web_ui/server/navigation/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from navigation.views import WaypointView, SaveCurrentPoseView, DeleteWaypointView, GoToWaypointView, CancelWaypointView, SetCurrentPoseWaypointView
 
 urlpatterns = [
-    url(r'^$', WaypointView.as_view(), name='index'),
+    url(r'^$', WaypointView.as_view(), name='navigation'),
     url(r'^save_current_pose', SaveCurrentPoseView.as_view(), name='save_current_pose'),
     url(r'^delete_waypoint', DeleteWaypointView.as_view(), name='delete_waypoint'),
     url(r'^goto_waypoint', GoToWaypointView.as_view(), name='goto_waypoint'),
