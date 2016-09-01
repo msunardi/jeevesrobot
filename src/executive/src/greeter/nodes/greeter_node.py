4132595bab183baac87e94489df14f5faccf8b0a
Rick Armstrong <richarda@pdx.edu>
2016-08-31 23:55:39 -0700

    Greeting state now waits for a tour request.issue_5898

--- src/executive/src/greeter/nodes/greeter_node.py
+++ src/executive/src/greeter/nodes/greeter_node.py
@@ -14,17 +14,20 @@ class GreetingState(smach.State):
         smach.State.__init__(self, outcomes=['tour_requested'])
         self.tour_requested = False
 
-    def begin_tour(self, req):
-        pass
+    def begin_tour(self):
+        self.tour_requested = True
 
     def execute(self, userdata):
-        if not self.tour_requested:
+        while not self.tour_requested:
+            print("Smiling and awaiting a tour request.")
             time.sleep(5)
+        self.tour_requested = False  # re-arm
         return 'tour_requested'
 
 
 class Greeter(threading.Thread):
     def __init__(self):
+        self.states = {}
         self.sleeper = rospy.Rate(1)
         rospy.Service('/greeter/begin_tour',
                       BeginTour,
@@ -34,20 +37,25 @@ class Greeter(threading.Thread):
         # create the top-level state machine
         self.sm_top = smach.StateMachine(outcomes=['idle'])
         with self.sm_top:
-            smach.StateMachine.add('GREETING', GreetingState(),
-                                   transitions={'tour_requested': 'GIVING_TOUR'})
+            self.states['greeting_state'] = smach.StateMachine.add(
+                'GREETING', GreetingState(),
+                transitions={'tour_requested': 'GIVING_TOUR'})
 
-            # add sub-states
+            # GIVING_TOUR container
             sm_giving_tour = smach.StateMachine(outcomes=['AT_HOME'])
+            # add sub-states
             with sm_giving_tour:
-                smach.StateMachine.add('IN_TRANSIT', tg.InTransitState(),
-                                       transitions={'arrived_at_poi': 'GIVING_SPIEL',
-                                                    'arrived_at_home': 'AT_HOME'})
-                smach.StateMachine.add('GIVING_SPIEL', tg.GivingSpielState(),
-                                       transitions={'spiel_complete': 'IN_TRANSIT'})
+                smach.StateMachine.add(
+                    'IN_TRANSIT', tg.InTransitState(),
+                    transitions={'arrived_at_poi': 'GIVING_SPIEL',
+                                 'arrived_at_home': 'AT_HOME'})
+                smach.StateMachine.add(
+                    'GIVING_SPIEL', tg.GivingSpielState(),
+                    transitions={'spiel_complete': 'IN_TRANSIT'})
 
-            smach.StateMachine.add('GIVING_TOUR', sm_giving_tour,
-                                   transitions={'AT_HOME': 'GREETING'})
+            smach.StateMachine.add(
+                'GIVING_TOUR', sm_giving_tour,
+                transitions={'AT_HOME': 'GREETING'})
 
         # Start an IntrospectionServer so we can examine
         # the running state machine
@@ -59,6 +67,7 @@ class Greeter(threading.Thread):
         self.sm_top.execute()
 
     def handle_begin_tour(self, req):
+        self.states['greeting_state'].begin_tour()
         return 0
 
     def run(self):
