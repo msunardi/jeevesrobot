0a6ae65cc776591e359acb3a93358f07745afbf1
Rick Armstrong <richarda@pdx.edu>
2016-08-31 23:02:27 -0700

    Implemented skeleton of BeginTour. Added some more flesh to the simulator.issue_5898

--- src/executive/src/greeter/nodes/greeter_node.py
+++ src/executive/src/greeter/nodes/greeter_node.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import rospy
@@ -11,52 +12,63 @@ from greeter.srv import *
 class GreetingState(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['tour_requested'])
-
-        rospy.Service('/greeter/begin_tour',
-                      BeginTour,
-                      self.begin_tour)
+        self.tour_requested = False
 
     def begin_tour(self, req):
         pass
 
     def execute(self, userdata):
-        time.sleep(5)
+        if not self.tour_requested:
+            time.sleep(5)
         return 'tour_requested'
 
 
-def main():
-    rospy.init_node('greeter_executive_node')
+class Greeter(threading.Thread):
+    def __init__(self):
+        self.sleeper = rospy.Rate(1)
+        rospy.Service('/greeter/begin_tour',
+                      BeginTour,
+                      self.handle_begin_tour)
+        threading.Thread.__init__(self)
 
-    # Create the top level SMACH state machine
-    sm_top = smach.StateMachine(outcomes=['idle'])
+        # create the top-level state machine
+        self.sm_top = smach.StateMachine(outcomes=['idle'])
+        with self.sm_top:
+            smach.StateMachine.add('GREETING', GreetingState(),
+                                   transitions={'tour_requested': 'GIVING_TOUR'})
 
-    # Open the container
-    with sm_top:
-        smach.StateMachine.add('GREETING', GreetingState(),
-                               transitions={'tour_requested': 'GIVING_TOUR'})
+            # add sub-states
+            sm_giving_tour = smach.StateMachine(outcomes=['AT_HOME'])
+            with sm_giving_tour:
+                smach.StateMachine.add('IN_TRANSIT', tg.InTransitState(),
+                                       transitions={'arrived_at_poi': 'GIVING_SPIEL',
+                                                    'arrived_at_home': 'AT_HOME'})
+                smach.StateMachine.add('GIVING_SPIEL', tg.GivingSpielState(),
+                                       transitions={'spiel_complete': 'IN_TRANSIT'})
 
-        # Create the sub SMACH state machine
-        sm_giving_tour = smach.StateMachine(outcomes=['AT_HOME'])
+            smach.StateMachine.add('GIVING_TOUR', sm_giving_tour,
+                                   transitions={'AT_HOME': 'GREETING'})
 
-        # Open the container
-        with sm_giving_tour:
-            # Add states to the container
-            smach.StateMachine.add('IN_TRANSIT', tg.InTransitState(),
-                                   transitions={'arrived_at_poi': 'GIVING_SPIEL',
-                                                'arrived_at_home': 'AT_HOME'})
-            smach.StateMachine.add('GIVING_SPIEL', tg.GivingSpielState(),
-                                   transitions={'spiel_complete': 'IN_TRANSIT'})
+        # Start an IntrospectionServer so we can examine
+        # the running state machine
+        sis = smach_ros.IntrospectionServer('sm_viewer_server', self.sm_top,
+                                            '/SM_ROOT')
+        sis.start()
 
-        smach.StateMachine.add('GIVING_TOUR', sm_giving_tour,
-                               transitions={'AT_HOME': 'GREETING'})
+        # start the top-level state machine
+        self.sm_top.execute()
 
-    # Execute plan
-    sis = smach_ros.IntrospectionServer('sm_viewer_server',
-                                        sm_top,
-                                        '/SM_ROOT')
-    sis.start()
-    sm_top.execute()
-    rospy.spin()
+    def handle_begin_tour(self, req):
+        return 0
+
+    def run(self):
+        self.sleeper.sleep()
+        while not rospy.is_shutdown():
+            pass
+
 
 if __name__ == '__main__':
-    main()
+    rospy.init_node('greeter_executive_node')
+    greeter = Greeter()
+    greeter.start()
+    rospy.spin()
