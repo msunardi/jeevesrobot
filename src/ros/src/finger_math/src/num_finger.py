8a6d12d46b2b534b9440f173f3d1b9709e77ea5e
Rick Armstrong <richarda@pdx.edu>
2017-03-20 21:24:04 -0700

    Add shoulder movements

--- src/ros/src/finger_math/src/num_finger.py
+++ src/ros/src/finger_math/src/num_finger.py
@@ -12,7 +12,7 @@ class Num_Finger(threading.Thread):
 
 	def __init__(self):
 		self.sub = rospy.Subscriber("finger_solved", String, self.callback)
-		self.finger_pub = rospy.Publisher('/pololu/command', MotorCommand, queue_size=10)
+		self.pub = rospy.Publisher('/pololu/command', MotorCommand, queue_size=10)
 
 		self.r_pinkie = MotorCommand()
 		self.r_pinkie.joint_name = 'r_hand_to_pinkie'
@@ -47,25 +47,62 @@ class Num_Finger(threading.Thread):
 		self.l_ring = MotorCommand()
 		self.l_ring.joint_name = 'l_hand_to_ring'
 		self.l_ring.speed = 0.8
-		self.l_ring.acceleration = 0.2
+		self.l_ring.acceleration = 1.0
 
 		self.l_middle = MotorCommand()
 		self.l_middle.joint_name = 'l_hand_to_middle'
 		self.l_middle.speed = 0.8
-		self.l_middle.acceleration = 0.2
+		self.l_middle.acceleration = 1.0
 
 		self.l_index = MotorCommand()
 		self.l_index.joint_name = 'l_hand_to_index'
 		self.l_index.speed = 0.8
-		self.l_index.acceleration = 0.3
+		self.l_index.acceleration = 1.0
 
 		self.l_thumb = MotorCommand()
 		self.l_thumb.joint_name = 'l_hand_to_thumb'
-		self.l_thumb.speed = 0.8
+		self.l_thumb.speed = 0.6
 		self.l_thumb.acceleration = 0.3
+
+		self.r_base_one = MotorCommand()
+		self.r_base_one.joint_name = 'base_to_r_arm_one'
+		self.r_base_one.speed = 0.8
+		self.r_base_one.acceleration = 0.3
+
+		self.r_one_two = MotorCommand()
+		self.r_one_two.joint_name = 'r_arm_one_to_arm_two'
+		self.r_one_two.speed = 1.0
+		self.r_one_two.acceleration = 0.3
+
+		self.r_two_three = MotorCommand()
+		self.r_two_three.joint_name = 'r_arm_two_to_arm_three'
+		self.r_two_three.speed = 0.7
+		self.r_two_three.acceleration = 0.3
+
+		self.l_base_one = MotorCommand()
+		self.l_base_one.joint_name = 'base_to_l_arm_one'
+		self.l_base_one.speed = 0.8
+		self.l_base_one.acceleration = 0.3
+
+		self.l_one_two = MotorCommand()
+		self.l_one_two.joint_name = 'l_arm_one_to_arm_two'
+		self.l_one_two.speed = 1.0
+		self.l_one_two.acceleration = 0.3
+
+		self.l_two_three = MotorCommand()
+		self.l_two_three.joint_name = 'l_arm_two_to_arm_three'
+		self.l_two_three.speed = 0.7
+		self.l_two_three.acceleration = 0.3
 
 		self.fingers = [self.l_thumb, self.l_index, self.l_middle, self.l_ring, self.l_pinkie, \
                                 self.r_index, self.r_thumb, self.r_middle, self.r_ring, self.r_pinkie]
+
+		self.shoulders = [self.r_base_one, self.r_one_two, self.r_two_three, \
+ 				  self.l_base_one, self.l_one_two, self.l_two_three]
+
+		self.shoulder_open = [1.0, 0.7, 0.0, 1.0, 0.8, 1.3]
+		self.shoulder_rest = [0.0, 0.0, -1.0, 0.0, 0.0, -1.0]
+
 		self.ALL = range(5)
 		self.ALL += [7, 6, 5, 8, 9]
 
@@ -191,13 +228,14 @@ class Num_Finger(threading.Thread):
 		# 	self.l_hand_to_pinkie.position = 45
 
 		
-		positions = [0.75, 0.75, 0.75, 0.75, 0.75]
+		positions = [0.75, 0.75, 0.70, 0.70, 0.70]
 		closed = self.ALL
 		opens = []
 		self.close_fingers(closed)
 		sleep(0.5)
 		self.open_fingers(self.ALL)
 		sleep(0.5)
+		self.move_shoulders(self.shoulder_open)
 		self.close_fingers(self.ALL)
 		closed = []
 
@@ -208,7 +246,13 @@ class Num_Finger(threading.Thread):
 			self.close_fingers(self.ALL)
 			return
 
+		if len(numbers) >= 2:
+			self.move_shoulders([0.0, 0.0, -1.0, 1.0, 1.0, 1.3])
+
 		for number in numbers:
+			if number in '012345':
+				self.move_shoulders([0.0, 0.0, -1.0, 1.0, 1.0, 1.3])
+			
 			if(number == '1'):
 				# closed = [4, 3, 2, 0]
 				# opens = [0]
@@ -250,6 +294,7 @@ class Num_Finger(threading.Thread):
 			#	opens += [9,8,7,6,5]
 			#	closed = [9]
 			elif number in '6789':
+				self.move_shoulders([1.0, 1.0, 0.8, 1.0, 1.0, 1.2])
 				opens = range(int(number))
 			elif(number == '0'):
 				opens = [2, 3, 4]
@@ -274,19 +319,27 @@ class Num_Finger(threading.Thread):
 		sleep(2*random())	
 		self.open_fingers(self.ALL)
 		self.close_fingers(self.ALL)
+		self.move_shoulders(self.shoulder_rest)
 
 	def close_fingers(self, close):
 		for c in close:
 			self.fingers[c].position = 0.0
-			self.finger_pub.publish(self.fingers[c])
+			self.pub.publish(self.fingers[c])
 			sleep(0.1)
 	
 	def open_fingers(self, opens):
 		for o in opens:
-			self.fingers[o].position = 0.70
+			self.fingers[o].position = 0.65
 			rospy.loginfo(self.fingers[o])
-			self.finger_pub.publish(self.fingers[o])
+			self.pub.publish(self.fingers[o])
 			sleep(0.1)	
+
+	def move_shoulders(self, openclose):
+		for s in zip(self.shoulders, openclose):
+			rospy.loginfo(s[0].joint_name)
+			s[0].position = s[1]
+			rospy.loginfo(s[0])
+			self.pub.publish(s[0])
 
 	def redis_handler(self, message):
 		rospy.loginfo("REDIS MESSAGE: %s" % message['data'])
